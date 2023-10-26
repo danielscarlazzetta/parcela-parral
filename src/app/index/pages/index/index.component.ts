79dc94cae146e2a2344d4b8f907bfe3139d9317d
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2023-10-26 01:42:51 -0300

    navbar terminado responsivo

--- src/app/index/pages/index/index.component.ts
+++ src/app/index/pages/index/index.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['../../../../styles.css'],
+  
 })
 export class IndexComponent {
 
