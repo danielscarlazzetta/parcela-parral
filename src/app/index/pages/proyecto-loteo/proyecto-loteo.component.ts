93cf10c4e57d811a11b851f58c3aa145347ff0c3
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2023-11-01 17:26:21 -0300

    loteo card terminado

--- src/app/index/pages/proyecto-loteo/proyecto-loteo.component.ts
+++ src/app/index/pages/proyecto-loteo/proyecto-loteo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { SharedServicesService } from '../../services/shared-services.service';
+import { LoteoDTO } from '../../interface/loteo.interface';
 
 
 @Component({
@@ -12,11 +13,11 @@ export class ProyectoLoteoComponent implements OnInit {
 
   private map: L.Map | any = null;
 
-
-  constructor(private miVariableIndex : SharedServicesService){}
+  constructor(private miVariableIndex: SharedServicesService) { }
 
   ngOnInit(): void {
     this.initializeMap();
+    this.datosParcela();
     console.log(this.miVariableIndex.getMiVariable())
   }
 
@@ -31,16 +32,67 @@ export class ProyectoLoteoComponent implements OnInit {
       .then((data) => {
         const geoJsonLayer = L.geoJSON(data, {
           style: {
-            fillColor: 'green', 
-            color: 'red',   
-            weight: 1       
+            fillColor: 'green',
+            color: 'red',
+            weight: 1
           }
         }).addTo(this.map);
 
         this.map.fitBounds(geoJsonLayer.getBounds());
       });
+  }
 
+  loteo: LoteoDTO[] = [];
+
+  private datosParcela() {
+    if (this.miVariableIndex.getMiVariable() === 'assets/json/el-bonito.json') {
+      console.log('todo pasando 1');
+      this.loteo = [
+        {
+          nombre: 'El bonito',
+          precio: '$6.800.000',
+          distancia: '25 km de Parral',
+          sector: 'camino talhuenes',
+          rol: 'Propio',
+          propiedades: 'Saneadas',
+          transferencia: 'Lista para transferir',
+          pendiente: 'Pendiente suabe',
+          divicion: 'Cuenta con estacas'
+        }
+      ];
+    }
+    if (this.miVariableIndex.getMiVariable() === 'assets/json/santa-teresa.json') {
+      console.log('todo pasando 2');
+      this.loteo = [
+        {
+          nombre: 'Santa Teresa',
+          precio: '$16.000.000',
+          distancia: '7 km de Retiro',
+          sector: 'Frente a tucapel',
+          rol: 'Propio',
+          propiedades: 'Saneadas',
+          transferencia: 'Lista para transferir',
+          pendiente: 'Sin pendiente',
+          divicion: 'Cuenta con estacas'
+        }
+      ];
+    }
+    if (this.miVariableIndex.getMiVariable() === 'assets/json/talhuenes.json') {
+      console.log('todo pasando 3');
+      this.loteo = [
+        {
+          nombre: 'Lomas de Machicura',
+          precio: '$10.000.000',
+          distancia: '23 km de Parral',
+          sector: 'Entre remulcado y camelia',
+          rol: 'Propio',
+          propiedades: 'Saneadas',
+          transferencia: 'Lista para transferir',
+          pendiente: 'Sin pendiente',
+          divicion: 'Cuenta con estacas'
+        }
+      ];
+    }
   }
 
 }
-
